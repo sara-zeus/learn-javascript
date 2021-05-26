de29463580cb6b29077f5e85f9e3368fd3087f17
sara-zeus <74381399+sara-zeus@users.noreply.github.com>
2021-05-26 14:19:29 -0600

    stars

---
+++ stars.js
@@ -0,0 +1,12 @@
+showStars(10);
+
+function showStars(rows){
+
+    for(let row=1; row<=rows; rows++){
+    let pattern ='';
+    for(let i=0; i<row; i++);
+    patttern +='*'; 
+    console.log(pattern);
+}
+
+}
